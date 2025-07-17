d3e1c7c19984555f9388d08eeddd4b79547aa44a
Eity2021 <Eity2021akter@gmail.com>
2025-07-17 21:35:48 +0600

    add changes

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -74,10 +74,7 @@ export function SignIn() {
           <Button className="mt-6 bg-[#121926]" fullWidth type="submit">
             Sign In
           </Button>
-          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-            Not registered?
-            <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
-          </Typography>
+
         </form>
 
       </div>
